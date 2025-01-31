7f7e844d6c53b9e72f072f00c3828d05532b3930
Aris <arishajderaj13@gmail.com>
2025-01-31 11:49:18 +0100

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#form1');
 
-form.addEventListener('action', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
